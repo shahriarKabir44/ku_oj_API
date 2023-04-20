8b1fd90df23dc64ee3afa04dfee9a533117b0a56
shahriarKabir44 <shahriar1904@cseku.ac.bd>
2023-04-20 10:18:13 +0600

    fixed bugs

--- repositories/User.repository.js
+++ repositories/User.repository.js
@@ -3,21 +3,22 @@ const QueryBuilder = require('../utils/queryBuilder')
 const jwt = require('jsonwebtoken')
 module.exports = class UserRepository {
     static async findUser(param, value) {
-        let [user] = await promisify(`select * from user where ${param}=?`, [value])
+        let [user] = await promisify({ sql: `select * from user where ${param}=?`, values: [value] })
         return user
     }
     static async register({ userName, password }) {
         try {
-            await promisify(
-                QueryBuilder.insertQuery('user', ['userName', 'password']),
-                [userName, password]
-            )
+            await promisify({
+                sql: QueryBuilder.insertQuery('user', ['userName', 'password']),
+                values: [userName, password]
+            })
             let user = await UserRepository.findUser('userName', userName)
             user.password = null
             let token = jwt.sign(user, process.env.jwtSecret)
             return { user, token }
 
         } catch (error) {
+            console.log(error)
             return null
         }
     }
