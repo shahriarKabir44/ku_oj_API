b3874b036045d06ab21967b2a088f5ac77f43a89
shahriarKabir44 <shahriar1904@cseku.ac.bd>
2023-04-27 00:21:00 +0600

    added judge logic

--- repositories/Judge.repository.js
+++ repositories/Judge.repository.js
@@ -11,6 +11,7 @@ module.exports = class JudgeRepository {
             console.log(data)
             responseObject.write(JSON.stringify(data))
 
+
         } catch (error) {
             responseObject.write(JSON.stringify(error))
 
@@ -24,28 +25,20 @@ module.exports = class JudgeRepository {
         })
         if (status == 1) { //when AC
 
-            this.setScoreWhenAccepted(problemId, userId, points)
+            this.setScoreWhenAccepted(problemId, userId, points, contestId)
         }
         else {
-
+            this.setScoreWhenRejected(problemId, userId, -5, contestId)
         }
     }
 
-    static async updateSubmissionResult(userId, problemId, points) {
-        Promisify({
-            sql: `INSERT 
-                            into submissionResult(
-                                contestantId,
-                                problemId,
-                                points
-                            )
-                        values(?, ?, ?) 
-                        on DUPLICATE key UPDATE points = points+?;`,
-            values: [userId, problemId, points, points]
-        })
+    static async setScoreWhenRejected(problemId, userId, points, contestId) {
+        this.updateSubmissionResult(userId, problemId, -points)
+        this.updateContestResult(contestId, userId, -points)
     }
 
-    static async setScoreWhenAccepted(problemId, userId, points) {
+
+    static async setScoreWhenAccepted(problemId, userId, points, contestId) {
         const [{ acCounter }] = await Promisify({
             sql: `select count(id) as acCounter from submission where verdict=AC and
                     problemId=? and submittedBy=?;`,
@@ -54,7 +47,28 @@ module.exports = class JudgeRepository {
         })
         if (acCounter == 1) {
             this.updateSubmissionResult(userId, problemId, points)
+            this.updateContestResult(contestId, userId, points)
         }
+    }
+    static async updateContestResult(contestId, contestantId, points) {
+        Promisify({
+            sql: `insert into contestResult(contestId,contestantId,points)
+                values(?,?,?) on DUPLICATE key UPDATE points = points+?;`,
+            values: [contestId, contestantId, points, points]
+        })
+    }
+
+    static async updateSubmissionResult(userId, problemId, points) {
+        Promisify({
+            sql: `INSERT into submissionResult(
+                                contestantId,
+                                problemId,
+                                points
+                            )
+                        values(?, ?, ?) 
+                        on DUPLICATE key UPDATE points = points+?;`,
+            values: [userId, problemId, points, points]
+        })
     }
 
     static calculatePoints(status) {
