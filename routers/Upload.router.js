2d6474dda828037f9b466b094e81ac3c2529b85b
shahriarKabir44 <shahriar1904@cseku.ac.bd>
2023-07-29 23:12:11 +0600

    fixed code view

--- routers/Upload.router.js
+++ routers/Upload.router.js
@@ -8,30 +8,6 @@ UploadRouter.post('/upload', upload.single('file'), (req, res) => {
 
 })
 
-UploadRouter.post('/storeContent', async (req, res) => {
-    await writeTestcaseFileContents(req.body.problemId, req.body.testcaseFileContent, 'in')
-    await writeTestcaseFileContents(req.body.problemId, req.body.outputFileContent, 'out')
-    res.send({ success: 1 })
-})
 
-
-function writeTestcaseFileContents(problemId, content, type) {
-    return new Promise((resolve, reject) => {
-        const filePath = getFileDir() + `/testcases/${problemId}`
-        fs.mkdir(filePath, { recursive: true }, (err) => {
-            if (err) {
-                console.error('Error creating directory:', err);
-            } else {
-                fs.writeFile(filePath + `/${type}.txt`, content, (err) => {
-                    if (err) {
-                        console.log(err)
-                    }
-                    resolve()
-                })
-            }
-        });
-
-    })
-}
 
 module.exports = UploadRouter
