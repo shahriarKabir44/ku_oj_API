9c328cb48457f983971acd3b29513985c0b4f653
shahriarKabir44 <shahriar1904@cseku.ac.bd>
2024-01-20 23:25:54 +0600

    re-enabled redis for localhost

--- utils/RedisClient.js
+++ utils/RedisClient.js
@@ -12,7 +12,6 @@ class RedisClient {
         if (process.env.redisPassword) {
             config['redisPassword'] = process.env.redisPassword
         }
-        console.log(config)
         const client = redis.createClient(config)
         this.client = client
 
@@ -21,31 +20,30 @@ class RedisClient {
 
     }
     static async queryCache(key) {
-        // let data = await this.client.get(key)
-        // if (data == null) return null
-        // return (JSON.parse(data))
-        return null
+        let data = await this.client.get(key)
+        if (data == null) return null
+        return (JSON.parse(data))
 
     }
     static async store(key, value) {
-        // if (!value) return
-        // try {
-        //     await this.remove(key)
-        //     return this.client.set(key, JSON.stringify(value), {
-        //         EX: 3600
-        //     })
+        if (!value) return
+        try {
+            await this.remove(key)
+            return this.client.set(key, JSON.stringify(value), {
+                EX: 3600
+            })
 
-        // } catch (error) {
-        //     console.log('store', error, key, value)
-        // }
-        //  this.client.expire(key, 3600 * 2 * 1000, (err, data) => { })
+        } catch (error) {
+            console.log('store', error, key, value)
+        }
+        this.client.expire(key, 3600 * 2 * 1000, (err, data) => { })
     }
     static async remove(key) {
-        // return new Promise((resolve, reject) => {
-        //     this.client.set(key, "1", 'NX').then((res) => {
-        //         resolve(res)
-        //     })
-        // })
+        return new Promise((resolve, reject) => {
+            this.client.set(key, "1", 'NX').then((res) => {
+                resolve(res)
+            })
+        })
     }
 }
 
