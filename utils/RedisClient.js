fd74cad44e593bd6ebe2c9693318f13a39da14f5
shahriarKabir44 <shahriar1904@cseku.ac.bd>
2024-01-20 23:20:45 +0600

    disabled redis

--- utils/RedisClient.js
+++ utils/RedisClient.js
@@ -21,31 +21,31 @@ class RedisClient {
 
     }
     static async queryCache(key) {
-        let data = await this.client.get(key)
-        if (data == null) return null
-        return (JSON.parse(data))
-
+        // let data = await this.client.get(key)
+        // if (data == null) return null
+        // return (JSON.parse(data))
+        return null
 
     }
     static async store(key, value) {
-        if (!value) return
-        try {
-            await this.remove(key)
-            return this.client.set(key, JSON.stringify(value), {
-                EX: 3600
-            })
+        // if (!value) return
+        // try {
+        //     await this.remove(key)
+        //     return this.client.set(key, JSON.stringify(value), {
+        //         EX: 3600
+        //     })
 
-        } catch (error) {
-            console.log('store', error, key, value)
-        }
+        // } catch (error) {
+        //     console.log('store', error, key, value)
+        // }
         //  this.client.expire(key, 3600 * 2 * 1000, (err, data) => { })
     }
     static async remove(key) {
-        return new Promise((resolve, reject) => {
-            this.client.set(key, "1", 'NX').then((res) => {
-                resolve(res)
-            })
-        })
+        // return new Promise((resolve, reject) => {
+        //     this.client.set(key, "1", 'NX').then((res) => {
+        //         resolve(res)
+        //     })
+        // })
     }
 }
 
