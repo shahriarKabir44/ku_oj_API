220c16b34ac0d7267a876f995632bec7644bd2f0
Saiful Islam Reyad <riyadsaif18@gmail.com>
2026-02-25 13:59:47 +0600

    multiple test cases are working but need some fixes

--- utils/fileManager.js
+++ utils/fileManager.js
@@ -1,92 +1,90 @@
-const multer = require('multer')
+const multer = require("multer");
 
-const fs = require('fs')
+const fs = require("fs");
 const storage = multer.diskStorage({
-    destination: (req, res, cb) => cb(null, getUploadFilePath(req)[1]),
-    filename: (req, res, cb) => cb(null, getUploadedFileName(req))
-})
+  destination: (req, res, cb) => cb(null, getUploadFilePath(req)[1]),
+  filename: (req, res, cb) => cb(null, getUploadedFileName(req)),
+});
 
 function getUploadFilePath(req) {
-    const { filetype, problemid } = req.headers
-    let path = 'executors/'
-    let tempPath = ''
-    if (filetype == 'submission') {
-        const { postedby, contestid } = req.headers
-        path += `/submissions/${contestid}/${postedby}/${problemid}`
-        tempPath += `/submissions/${contestid}/${postedby}/${problemid}`
-        const { submissionid, ext } = req.headers
+  const { filetype, problemid } = req.headers;
+  let path = "executors/";
+  let tempPath = "";
+  if (filetype == "submission") {
+    const { postedby, contestid } = req.headers;
+    path += `/submissions/${contestid}/${postedby}/${problemid}`;
+    tempPath += `/submissions/${contestid}/${postedby}/${problemid}`;
+    const { submissionid, ext } = req.headers;
 
-        if (ext == 'cpp' || ext == 'java') {
-            path += `/${submissionid}`
-            tempPath += `/${submissionid}`
-        }
+    if (ext == "cpp" || ext == "java") {
+      path += `/${submissionid}`;
+      tempPath += `/${submissionid}`;
     }
-    else if (filetype == 'testcaseoutput' || filetype == 'testcaseinput') {
-        path += `/testcases/${problemid}`
-        tempPath += `/testcases/${problemid}`
-    }
-    else if (filetype == 'statementfile') {
-        path = `problemStatements/`
-        tempPath = ``
+  } else if (filetype == "testcaseoutput" || filetype == "testcaseinput") {
+    path += `/testcases/${problemid}`;
+    tempPath += `/testcases/${problemid}`;
+  } else if (filetype == "statementfile") {
+    path = `problemStatements/`;
+    tempPath = ``;
+  } else return null;
 
-    }
-    else return null
-
-    if (!fs.existsSync(path)) {
-        fs.mkdirSync(path, { recursive: true });
-    }
-    req.fileDir = tempPath
-    return [tempPath, path]
+  if (!fs.existsSync(path)) {
+    fs.mkdirSync(path, { recursive: true });
+  }
+  req.fileDir = tempPath;
+  return [tempPath, path];
 }
 
 function getUploadedFileName(req, submissionid = 0) {
-    const { filetype, ext } = req.headers
-    let filename = ""
-    if (filetype == 'submission') {
-        filename = submissionid
-        if (req.headers.ext == 'java') {
-            filename = 'Solution'
-        }
+  const { filetype, ext } = req.headers;
+  let filename = "";
+  if (filetype == "submission") {
+    filename = submissionid;
+    if (req.headers.ext == "java") {
+      filename = "Solution";
     }
-    else if (filetype == 'testcaseinput') {
-
-        filename = 'in'
-    }
-
-    else if (filetype == 'testcaseoutput') {
-
-        filename = 'out'
-    }
-    else if (filetype == 'statementfile') {
-        const { problemid } = req.headers
-        filename = problemid
-    }
-    else {
-        return null;
-    }
-    req.filename = `${filename}.${ext}`
-    return `${filename}.${ext}`
-
+  } else if (filetype == "testcaseinput") {
+    const { testcaseindex } = req.headers;
+    let suffix =
+      testcaseindex !== undefined && testcaseindex !== ""
+        ? `_${testcaseindex}`
+        : "";
+    filename = `in${suffix}`;
+  } else if (filetype == "testcaseoutput") {
+    const { testcaseindex } = req.headers;
+    let suffix =
+      testcaseindex !== undefined && testcaseindex !== ""
+        ? `_${testcaseindex}`
+        : "";
+    filename = `out${suffix}`;
+  } else if (filetype == "statementfile") {
+    const { problemid } = req.headers;
+    filename = problemid;
+  } else {
+    return null;
+  }
+  req.filename = `${filename}.${ext}`;
+  return `${filename}.${ext}`;
 }
 
 // allowed mimetypes: images and pdf
 const ALLOWED_MIMETYPES = [
-    'application/pdf',
-    'image/png',
-    'image/jpeg',
-    'image/jpg',
-    'image/gif',
-    'image/webp',
-    'image/svg+xml',
-    'text/plain'
-]
+  "application/pdf",
+  "image/png",
+  "image/jpeg",
+  "image/jpg",
+  "image/gif",
+  "image/webp",
+  "image/svg+xml",
+  "text/plain",
+];
 
 function fileFilter(req, file, cb) {
-    if (ALLOWED_MIMETYPES.includes(file.mimetype)) cb(null, true)
-    else cb(null, false)
+  if (ALLOWED_MIMETYPES.includes(file.mimetype)) cb(null, true);
+  else cb(null, false);
 }
 
 // limit to a single file upload
-const upload = multer({ storage, fileFilter, limits: { files: 1 } })
+const upload = multer({ storage, fileFilter, limits: { files: 1 } });
 
 module.exports = { upload, getUploadFilePath, getUploadedFileName };
